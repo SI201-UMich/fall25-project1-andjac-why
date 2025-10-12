62f9e869b2ae61a3f790fd7497033526c1001ae1
andjac <andjac@umich.edu>
2025-10-11 23:15:19 -0400

    Made it so the beal_average function finds the average for a penguins species(not hardcoded as to test).

--- Project_1.py
+++ Project_1.py
@@ -1,28 +1,33 @@
 import csv
 
-
-
 def penguin_pullinfo(penguin_csv):
-    d = []
-    with open(penguin_csv, "r") as inFile:
+    rows = []
+    with open(penguin_csv, "r", newline="") as inFile: 
         reader = csv.DictReader(inFile)   
-
         for row in reader:
-            d.append(row)
-        return d
-    
+            rows.append(row)
+    return rows   
 
-def beak_average(d):
-    total = 0
+def beak_average(rows, species_name):
+    total = 0.0
     count = 0
     
-    for row in d:  
-        beak_len = row['bill_length_mm']
-        if beak_len != "NA":              
-            total += float(beak_len)      
-            count += 1                  
+    for row in rows:  
+        if row['species'] == species_name:
+            beak_len = row['bill_length_mm']
+            if beak_len != "NA":
+                total += float(beak_len)
+                count += 1
     
     if count > 0:
-        return total / count              
+        return total / count
     else:
         return None
+
+
+penguin_csv = r"C:/Users/User/Desktop/SI 201/Project 1 Checkpoint/penguins.csv"
+data = penguin_pullinfo(penguin_csv)
+adelie_avg = beak_average(data, "Adelie")
+print("Adelie avg bill length:", adelie_avg)
+gentoo_avg = beak_average(data, "Gentoo")
+print("Gentoo avg bill length:", gentoo_avg)
