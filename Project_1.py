146e6732bdde9e285d3b45e744c0538fa8187638
andjac <andjac@umich.edu>
2025-10-13 13:37:43 -0400

    Got rid of the test cases for now, added island parameter to both functions.

--- Project_1.py
+++ Project_1.py
@@ -9,15 +9,15 @@ def penguin_pullinfo(penguin_csv):
             rows.append(row)
     return rows   
 
-def bill_average_length(rows, species_name): #changed to bill, since I thought it said beak initially in the CSV file
+def bill_average_length(rows, species_name, island): #changed to bill, since I thought it said beak initially in the CSV file
     total = 0.0
     count = 0
     
-    for row in rows:  
-        if row['species'] == species_name:
-            beak_len = row['bill_length_mm']
-            if beak_len != "NA":
-                total += float(beak_len)
+    for row in rows:
+        if row['species'] == species_name and row['island'] == island:
+            length = row['bill_length_mm']
+            if length != "NA":
+                total += float(length)
                 count += 1
     
     if count > 0:
@@ -25,15 +25,15 @@ def bill_average_length(rows, species_name): #changed to bill, since I thought i
     else:
         avg = None
     
-    return [{"species": species_name, "average_bill_length": avg, "count": count}]
+    return [{"species": species_name,"island": island, "average_bill_length": avg,"count": count}]
 
     
-def bill_average_depth(rows, species_name):
+def bill_average_depth(rows, species_name, island ):
     total = 0.0
     count = 0
     
     for row in rows:
-        if row['species'] == species_name:
+        if row['species'] == species_name and row['island'] == island:
             depth = row['bill_depth_mm']
             if depth != "NA":
                 total += float(depth)
@@ -44,24 +44,11 @@ def bill_average_depth(rows, species_name):
     else:
         avg = None
     
-    return [{"species": species_name, "average_bill_depth": avg, "count": count}]
+    return [{"species": species_name,"island": island,"average_bill_depth": avg,"count": count}]
 
-class TestPenguinFunctions(unittest.TestCase):
-
-    def setUp(self):
-        
-        self.rows = [
-            {"species": "Adelie", "bill_length_mm": "39.1", "bill_depth_mm": "18.7"},
-            {"species": "Adelie", "bill_length_mm": "40.0", "bill_depth_mm": "19.0"},
-            {"species": "Adelie", "bill_length_mm": "NA",   "bill_depth_mm": "NA"},
-            {"species": "Gentoo", "bill_length_mm": "50.0", "bill_depth_mm": "14.0"}
-        ]
-
-    
-    def test_bill_average_length_general(self):
-        avg = bill_average_length(self.rows, "Adelie")[0]['average_bill_length']
-        self.assertEqual(avg, (39.1 + 40.0) / 2) 
-
-
-if __name__ == "__main__":
-    unittest.main()
+penguin_csv = r"C:/Users/User/Desktop/SI 201/Project 1 Checkpoint/penguins.csv"
+data = penguin_pullinfo(penguin_csv)
+adelie_torg_len = bill_average_length(data, "Adelie", "Torgersen")
+adelie_torg_depth = bill_average_depth(data, "Adelie", "Torgersen")
+print(adelie_torg_len)
+print(adelie_torg_depth)
