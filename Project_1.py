e2a2bdb2a4523b66707f080083d23c14d3dc1322
andjac <andjac@umich.edu>
2025-10-13 10:41:17 -0400

    Added function for finding the average bill depth for penguins.

--- Project_1.py
+++ Project_1.py
@@ -8,7 +8,7 @@ def penguin_pullinfo(penguin_csv):
             rows.append(row)
     return rows   
 
-def beak_average(rows, species_name):
+def bill_average_length(rows, species_name): #changed to bill, since I thought it said beak initially in the CSV file
     total = 0.0
     count = 0
     
@@ -23,11 +23,27 @@ def beak_average(rows, species_name):
         return total / count
     else:
         return None
+    
+def bill_average_depth(rows, species_name):
+    total = 0.0
+    count = 0
+    
+    for row in rows:
+        if row['species'] == species_name:
+            depth = row['bill_depth_mm']
+            if depth != "NA":
+                total += float(depth)
+                count += 1
+    
+    if count > 0:
+        return total / count
+    else:
+        return None
 
 
 penguin_csv = r"C:/Users/User/Desktop/SI 201/Project 1 Checkpoint/penguins.csv"
 data = penguin_pullinfo(penguin_csv)
-adelie_avg = beak_average(data, "Adelie")
+adelie_avg = bill_average_length(data, "Adelie")
 print("Adelie avg bill length:", adelie_avg)
-gentoo_avg = beak_average(data, "Gentoo")
+gentoo_avg = bill_average_length(data, "Gentoo")
 print("Gentoo avg bill length:", gentoo_avg)
